8f6af18296ec634de01aa4473ad935ff7a07a7e1
Salbul-mo <salbul.mo91@gmail.com>
2025-04-21 15:23:35 +0900

    [fix] 정적 페이지에 맞게 수정 test

--- app/sudoku/page.tsx
+++ app/sudoku/page.tsx
@@ -2,44 +2,76 @@
 
 import "../globals.css"
 import Link from "next/link"
-import { numberGenerator } from '../../components/sudoku/function/numberGen'
 
-// 버튼 태그에 클릭 핸들러와 숫자 값, 숫자의 위치를 삽입한다.
-function Box({ value, className, onBoxClick }: { value: number, className: string, onBoxClick: any }) {
-
-    return (<button className={className} onClick={onBoxClick}>
-        {value}
-    </button>
-
-    )
-
-}
-
-// 3 X 3 스도쿠 박스를 반환한다.
-function SudokuBox({ row, col }: { row: number, col: number }) {
-
-    const set1 = numberGenerator()
-
-    function handleClick(arr: number[]) {
-        // arr => row, col, index
-        return
-    }
-
-
-    return (
-        <div className="boxSet">
-            {set1.map((item, index) => {
-                return (
-                    <Box value={item} className={`sudokuBox ${index < 3 ? 'border-t-tokyo_night-50 border-t-3' : ''} ${6 <= index && index <= 8 ? 'border-b-tokyo_night-50 border-b-3' : ''} ${index % 3 === 0 ? 'border-l-tokyo_night-50 border-l-3' : ''} ${index % 3 === 2 ? 'border-r-tokyo_night-50 border-r-3' : ''} groupBox-${3 * (row - 1) + col} groupRow-${row - 1 !== 2 ? 3 * (row - 1) + Math.floor(index / 3) + 1 : 2 * row + Math.floor(index / 3) + 1} groupCol-${col - 1 !== 2 ? 3 * (col - 1) + index % 3 + 1 : 2 * col + index % 3 + 1} `} key={`${row} -${col} -${index + 1}`} onBoxClick={() => handleClick([row, col, index])} />
-                )
-            })}
-        </div >
-
-    )
-}
 
 // 완성된 스도쿠 판을 반환한다.
 export default function Sudoku() {
+
+    function numberGenerator(): number[] {
+
+        let array = getOneSet()
+
+        return array
+    }
+
+    // 최댓값이 주어지지 않으면 1 ~ 9 중 하나 반환
+    // 최댓값이 주어지면 1 ~ 최댓값 이하 중 하나 반환
+    function getRandomInt(max: number | null): number {
+
+        const number = Math.floor(Math.random() * (max ? max : 9)) + 1
+        return number
+    }
+
+    function getOneSet(): number[] {
+
+        const array: number[] = []
+
+        while (array.length < 9) {
+            let temp: number = getRandomInt(null)
+            if (array.includes(temp)) {
+                continue
+            } else {
+                array.push(temp)
+            }
+
+        }
+
+        return array
+    }
+
+    // 버튼 태그에 클릭 핸들러와 숫자 값, 숫자의 위치를 삽입한다.
+    function Box({ value, className, onBoxClick }: { value: number, className: string, onBoxClick: any }) {
+
+        return (<button className={className} onClick={onBoxClick}>
+            {value}
+        </button>
+
+        )
+
+    }
+
+    // 3 X 3 스도쿠 박스를 반환한다.
+    function SudokuBox({ row, col }: { row: number, col: number }) {
+
+        const set1 = numberGenerator()
+
+        function handleClick(arr: number[]) {
+            // arr => row, col, index
+            return
+        }
+
+
+        return (
+            <div className="boxSet">
+                {set1.map((item, index) => {
+                    return (
+                        <Box value={item} className={`sudokuBox ${index < 3 ? 'border-t-tokyo_night-50 border-t-3' : ''} ${6 <= index && index <= 8 ? 'border-b-tokyo_night-50 border-b-3' : ''} ${index % 3 === 0 ? 'border-l-tokyo_night-50 border-l-3' : ''} ${index % 3 === 2 ? 'border-r-tokyo_night-50 border-r-3' : ''} groupBox-${3 * (row - 1) + col} groupRow-${row - 1 !== 2 ? 3 * (row - 1) + Math.floor(index / 3) + 1 : 2 * row + Math.floor(index / 3) + 1} groupCol-${col - 1 !== 2 ? 3 * (col - 1) + index % 3 + 1 : 2 * col + index % 3 + 1} `} key={`${row} -${col} -${index + 1}`} onBoxClick={() => handleClick([row, col, index])} />
+                    )
+                })}
+            </div >
+
+        )
+    }
     return (
         <div className="container mx-auto flex flex-col items-center">
             <Link className="text-tokyo_green-500 text-3xl" href="/">Home </Link>
