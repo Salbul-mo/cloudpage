54a5ebd7deca292afaf0f951ccf0bc4ddcac760c
Salbul-mo <salbul.mo91@gmail.com>
2025-10-14 13:26:04 +0900

    [fix] 페이지 요소 수정

--- app/kieco_receipt_list/page.tsx
+++ app/kieco_receipt_list/page.tsx
@@ -316,7 +316,7 @@ const ReceiptListPage: React.FC = () => {
                   금액
                 </th>
                 <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  거래처사업자번호
+                  사업자번호
                 </th>
                 <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                   상호명
